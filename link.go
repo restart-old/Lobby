9d7c5230eae4f20212d5ccf739c4f3c76ae946c5
RestartFU <zerkoming@gmail.com>
2021-12-15 12:47:51 -0800

    handle link errors

--- link.go
+++ link.go
@@ -16,15 +16,19 @@ type LinkCommand struct {
 func (l LinkCommand) Run(src cmd.Source, o *cmd.Output) {
 	if p, ok := src.(*player.Player); ok {
 		var code link.Code
-		if r, ok := l.linker.LinkedFromGamerTag(src.Name()); !ok {
+		if r, ok, err := l.linker.LinkedFromGamerTag(src.Name()); !ok {
+			if err != nil {
+				p.Message("§cAn error occurred, please notify staff!")
+				return
+			}
 			if code, ok = l.linker.LoadByUser(src.Name()); !ok {
 				code = link.NewCode(7)
 				l.linker.Storer.Store(src.Name(), code)
 			}
 			until := time.Until(code.Expiration)
-			p.Messagef("Your code is %s and it will expire in %s.", code.Code, durafmt.Parse(until).LimitFirstN(2))
+			p.Messagef("§aYour code is %s and it will expire in %s.", code.Code, durafmt.Parse(until).LimitFirstN(2))
 		} else {
-			p.Messagef("You are already linked with the ID: %s, use /unlink if you wish to link again.", r.DiscordID())
+			p.Messagef("§cYou are already linked with the ID: %s, use /unlink if you wish to link again.", r.DiscordID())
 		}
 	}
 }
@@ -35,12 +39,16 @@ type UnlinkCommand struct {
 
 func (l UnlinkCommand) Run(src cmd.Source, o *cmd.Output) {
 	if p, ok := src.(*player.Player); ok {
-		if r, ok := l.linker.LinkedFromGamerTag(src.Name()); ok {
+		if r, ok, err := l.linker.LinkedFromGamerTag(src.Name()); ok {
+			if err != nil {
+				p.Message("§cAn error occurred, please notify staff!")
+				return
+			}
 			if r.LinkedSince().Add(21600 * time.Minute).Before(time.Now()) {
 				l.linker.UnLink(src.Name())
-				p.Message("You have been unlinked from the ID: %s.", r.DiscordID())
+				p.Message("§aYou have been unlinked from the ID: %s.", r.DiscordID())
 			} else {
-				p.Messagef("You are on unlink cooldown, wait %s.", durafmt.Parse(time.Until(r.LinkedSince().Add(21600*time.Minute))).LimitFirstN(1))
+				p.Messagef("§cYou are on unlink cooldown, wait %s.", durafmt.Parse(time.Until(r.LinkedSince().Add(21600*time.Minute))).LimitFirstN(1))
 			}
 		}
 	}
